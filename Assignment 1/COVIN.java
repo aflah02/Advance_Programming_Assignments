728461c338ea05052934c5c9a0609019c3b41de8
Aflah <aflah20082@iiitd.ac.in>
2021-10-08 10:30:40 +0530

    Built Classes and Started Menu Work with Basic Functionalities (No Error Handling Yet)

--- Assignment 1/COVIN.java
+++ Assignment 1/COVIN.java
@@ -1,6 +1,72 @@
-public class COVIN {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class COVIN {
+    static HashMap<String, Vaccine> vaccineTracker = new HashMap<>();
+    static HashMap<String, Hospital> hospitalTracker = new HashMap<>();
+    static HashMap<String, Citizen> citizenTracker = new HashMap<>();
+    private static int Hospital_Unique_ID = 100000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("CoWin Portal initialized....\n" +
+                "---------------------------------\n" +
+                "1. Add Vaccine\n" +
+                "2. Register Hospital\n" +
+                "3. Register Citizen\n" +
+                "4. Add Slot for Vaccination\n" +
+                "5. Book Slot for Vaccination\n" +
+                "6. List all slots for a hospital\n" +
+                "7. Check Vaccination Status\n" +
+                "8. Exit\n" +
+                "---------------------------------");
+        System.out.print("Please choose one of the tasks above: ");
+        int option = sc.nextInt();
+        System.out.println();
+        while (option != 8){
+            if (option == 1){
+                System.out.print("Vaccine Name: ");
+                String vaccine_name = sc.next();
+                System.out.print("Number of Doses: ");
+                int number_of_doses = sc.nextInt();
+                System.out.print("Gap between Doses: ");
+                int gap_between_doses = sc.nextInt();
+                Vaccine vaccine = new Vaccine(vaccine_name, number_of_doses, gap_between_doses);
+                vaccineTracker.put(vaccine_name, vaccine);
+                System.out.println("Vaccine Name: " + vaccine_name + ", Number of Doses: " + number_of_doses + ", Gap Between Doses: " + gap_between_doses);
+                System.out.println("---------------------------------");
+            }
+            else if (option == 2){
+                System.out.print("Hospital Name: ");
+                String hospital_name = sc.next();
+                System.out.print("PinCode: ");
+                int PinCode = sc.nextInt();
+                int Unique_ID = Hospital_Unique_ID;
+                Hospital_Unique_ID ++;
+                Hospital hospital = new Hospital(hospital_name, PinCode, Unique_ID);
+                hospitalTracker.put(hospital_name, hospital);
+                System.out.println("Hospital Name: " + hospital_name + ", PinCode: " + PinCode + ", Unique ID: " + Unique_ID);
+                System.out.println("---------------------------------");
+            }
+            else if (option == 3){
+                System.out.print("Citizen Name: ");
+                String citizen_name = sc.next();
+                System.out.print("Age: ");
+                int citizen_age = sc.nextInt();
+                System.out.print("Unique ID: ");
+                long citizen_unique_id = sc.nextLong();
+                Citizen citizen = new Citizen(citizen_name, citizen_age, citizen_unique_id);
+                citizenTracker.put(citizen_name, citizen);
+                System.out.println("Citizen Name: " + citizen_name + ", Age: " + citizen_age + ", Unique ID: " + citizen_unique_id);
+                System.out.println("---------------------------------");
+            }
+            System.out.print("Please choose one of the tasks above: ");
+            option = sc.nextInt();
+        }
+        System.out.println("Thank You for Using our Program");
+    }
+
+    public static int getHospital_Unique_ID() {
+        return Hospital_Unique_ID;
     }
 }
 class Citizen{
@@ -31,6 +97,18 @@ class Vaccine{
         this.v_name = v_name;
         this.v_dose_count = v_dose_count;
         this.v_gap_between_doses = v_gap_between_doses;
+    }
+
+    public String getV_name() {
+        return v_name;
+    }
+
+    public int getV_dose_count() {
+        return v_dose_count;
+    }
+
+    public int getV_gap_between_doses() {
+        return v_gap_between_doses;
     }
 }
 class Slot{
---
+++ out/production/Advance_Programming_Assignments/observations.md
@@ -0,0 +1,46 @@
+Two chief stakeholders, citizens and hospitals
+
+### Hospitals
+
+- Register themselves in Portal
+- Upload their slots
+
+### Citizens
+
+- Register themselves on Portal
+- Look up vaccination slots based on Hospitals in an area and a specific vaccine
+- Should be able to book slots
+- Query portal for vaccination details
+- Query for next dose details
+
+### Slots
+
+- Day
+- Vaccine Given
+- Quantity in the slot
+- Hospital Id (look up)
+
+### Vaccine
+
+- Name
+- Number of Doses
+- Gap Between Doses
+
+### Citizen Registration
+
+- Name
+- Age
+- They give themselves **unique** 12 Digit IDs
+
+### Hospital Registration
+
+- Name
+- Pincode
+- Program gives them **unique** 6 Digit ID
+
+### COVIN Portal
+
+- Details of Citizens, Hospitals, and Vaccines
+- Vaccine Information must be added before Hospitals add their slots
+- Once booked assuming Vaccination is instantaneous
+- IMP: On a particular day and hospital, the  number of citizens registered for that slot does not exceed the maximum available slots, and  the chosen slot does not violate the stipulated duration between slots.
