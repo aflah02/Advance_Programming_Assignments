72d5fab6234d94122dadbd368adb9900947d7384
Aflah <aflah20082@iiitd.ac.in>
2021-10-08 20:44:43 +0530

    Finished Option 5 (No Error Handling Yet)

--- Assignment 1/COVIN.java
+++ Assignment 1/COVIN.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class COVIN {
     static HashMap<String, Vaccine> vaccineTracker = new HashMap<>();
     static ArrayList<String> vaccineNames = new ArrayList<>();
-    static HashMap<String, Hospital> hospitalTracker = new HashMap<>();
-    static HashMap<String, Citizen> citizenTracker = new HashMap<>();
-    static HashMap<Integer, Slot> slotTracker = new HashMap<Integer, Slot>();
+    static HashMap<Integer, Hospital> hospitalTracker = new HashMap<>();
+    static HashMap<Long, Citizen> citizenTracker = new HashMap<>();
+    static HashMap<Integer, ArrayList<Slot>> slotTracker = new HashMap<Integer, ArrayList<Slot>>();
     private static int Hospital_Unique_ID = 100000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -47,7 +48,7 @@ public class COVIN {
                 int Unique_ID = Hospital_Unique_ID;
                 Hospital_Unique_ID ++;
                 Hospital hospital = new Hospital(hospital_name, PinCode, Unique_ID);
-                hospitalTracker.put(hospital_name, hospital);
+                hospitalTracker.put(Unique_ID, hospital);
                 System.out.println("Hospital Name: " + hospital_name + ", PinCode: " + PinCode + ", Unique ID: " + Unique_ID);
                 System.out.println("---------------------------------");
             }
@@ -59,7 +60,7 @@ public class COVIN {
                 System.out.print("Unique ID: ");
                 long citizen_unique_id = sc.nextLong();
                 Citizen citizen = new Citizen(citizen_name, citizen_age, citizen_unique_id);
-                citizenTracker.put(citizen_name, citizen);
+                citizenTracker.put(citizen_unique_id, citizen);
                 System.out.println("Citizen Name: " + citizen_name + ", Age: " + citizen_age + ", Unique ID: " + citizen_unique_id);
                 System.out.println("---------------------------------");
             }
@@ -77,8 +78,17 @@ public class COVIN {
                     System.out.println(i + ". " + vaccineNames.get(i));
                 }
                 int vaccine_number =  sc.nextInt();
-                Slot slot = new Slot(day_number, vaccineNames.get(vaccine_number), quantity, hospital_id);
-                slotTracker.put(hospital_id, slot);
+                String vaccine_Name = vaccineNames.get(vaccine_number);
+                Slot slot = new Slot(day_number, vaccine_Name, quantity, hospital_id);
+                boolean isHospIDPresent = slotTracker.containsKey(hospital_id);
+                if (isHospIDPresent){
+                    slotTracker.get(hospital_id).add(slot);
+                }
+                else{
+                    slotTracker.put(hospital_id, new ArrayList<Slot>());
+                    slotTracker.get(hospital_id).add(slot);
+                }
+                Hospital hosp = hospitalTracker.get(hospital_id);
                 System.out.println("Slot added by Hospital " + hospital_id + " for Day: " + day_number + ", Available Quantity: " + quantity + " of Vaccine " + vaccineNames.get(vaccine_number));
                 System.out.println("---------------------------------");
             }
@@ -92,8 +102,77 @@ public class COVIN {
                 int choice = sc.nextInt();
                 if (choice == 1){
                     System.out.print("Enter Pincode: ");
-                    int n = sc.nextInt();
-
+                    int pincode = sc.nextInt();
+                    for (Map.Entry<Integer, ArrayList<Slot>> entry: slotTracker.entrySet()){
+                        for (int i = 0; i < entry.getValue().size(); i++){
+                            if (hospitalTracker.get(entry.getValue().get(i).getS_hospital_id()).getH_pincode() == pincode){
+                                System.out.println(entry.getValue().get(i).getS_hospital_id() + " " + hospitalTracker.get(entry.getValue().get(i).getS_hospital_id()).getH_name());
+                            }
+                        }
+                    }
+                    System.out.print("Enter hospital id: ");
+                    int hosp_id = sc.nextInt();
+                    for (int i = 0; i < slotTracker.get(hosp_id).size(); i++){
+                        Slot temp = slotTracker.get(hosp_id).get(i);
+                        System.out.println(i + "-> Day: " + temp.getS_day() + " Available Qty:" + temp.getS_quantity() + " Vaccine:" + temp.getS_vaccine());
+                    }
+                    System.out.print("Choose Slot: ");
+                    int chosenSlot = sc.nextInt();
+                    for (int i = 0; i < slotTracker.get(hosp_id).size(); i++){
+                        if (i == chosenSlot){
+                            slotTracker.get(hosp_id).get(i).reduceS_quantity();
+                            String vaccine_chosen = slotTracker.get(hosp_id).get(i).getS_vaccine();
+                            citizenTracker.get(patient_unique_id).setDoses(vaccineTracker.get(vaccine_chosen).getV_dose_count());
+                            citizenTracker.get(patient_unique_id).setDosesPending(vaccineTracker.get(vaccine_chosen).getV_dose_count());
+                            citizenTracker.get(patient_unique_id).DoseDone();
+                            citizenTracker.get(patient_unique_id).setVaccine_name(vaccine_chosen);
+                            citizenTracker.get(patient_unique_id).setVaccineGap(vaccineTracker.get(vaccine_chosen).getV_gap_between_doses());
+                            if (slotTracker.get(hosp_id).get(i).getS_quantity() == 0){
+                                slotTracker.get(hosp_id).remove(i);
+                            }
+                            break;
+                        }
+                    }
+                    System.out.println(citizenTracker.get(patient_unique_id) + " vaccinated with Covax");
+                    System.out.println("---------------------------------");
+                }
+                else if (choice == 2){
+                    System.out.print("Enter Vaccine name: ");
+                    String Vaccine_name = sc.next();
+                    for (Map.Entry<Integer, ArrayList<Slot>> entry: slotTracker.entrySet()){
+                        for (int i = 0; i < entry.getValue().size(); i++){
+                            if (entry.getValue().get(i).getS_vaccine().equals(Vaccine_name)){
+                                System.out.println(entry.getValue().get(i).getS_hospital_id() + " " + hospitalTracker.get(entry.getValue().get(i).getS_hospital_id()).getH_name());
+                            }
+                        }
+                    }
+                    System.out.print("Enter hospital id: ");
+                    int hosp_id = sc.nextInt();
+                    for (int i = 0; i < slotTracker.get(hosp_id).size(); i++){
+                        Slot temp = slotTracker.get(hosp_id).get(i);
+                        System.out.println(i + "-> Day: " + temp.getS_day() + " Available Qty:" + temp.getS_quantity() + " Vaccine:" + temp.getS_vaccine());
+                    }
+                    System.out.print("Choose Slot: ");
+                    int chosenSlot = sc.nextInt();
+                    for (int i = 0; i < slotTracker.get(hosp_id).size(); i++){
+                        if (i == chosenSlot){
+                            slotTracker.get(hosp_id).get(i).reduceS_quantity();
+                            String vaccine_chosen = slotTracker.get(hosp_id).get(i).getS_vaccine();
+                            citizenTracker.get(patient_unique_id).setDoses(vaccineTracker.get(vaccine_chosen).getV_dose_count());
+                            citizenTracker.get(patient_unique_id).setDosesPending(vaccineTracker.get(vaccine_chosen).getV_dose_count());
+                            citizenTracker.get(patient_unique_id).DoseDone();
+                            citizenTracker.get(patient_unique_id).setVaccine_name(vaccine_chosen);
+                            citizenTracker.get(patient_unique_id).setVaccineGap(vaccineTracker.get(vaccine_chosen).getV_gap_between_doses());
+                            if (slotTracker.get(hosp_id).get(i).getS_quantity() == 0){
+                                slotTracker.get(hosp_id).remove(i);
+                            }
+                            break;
+                        }
+                    }
+                    System.out.println(citizenTracker.get(patient_unique_id) + " vaccinated with Covax");
+                    System.out.println("---------------------------------");
+                    System.out.println(citizenTracker.get(patient_unique_id) + " vaccinated with Covax");
+                    System.out.println("---------------------------------");
                 }
 
             }
@@ -111,10 +190,35 @@ class Citizen{
     private String c_name;
     private int c_age;
     private long c_unique_id;
+    private int doses;
+    private int dosesPending;
+    private String vaccine_name;
+    private int vaccineGap;
+
+    public void setVaccineGap(int vaccineGap) {
+        this.vaccineGap = vaccineGap;
+    }
+
+    public void setVaccine_name(String vaccine_name) {
+        this.vaccine_name = vaccine_name;
+    }
+
     Citizen(String c_name, int c_age, long c_unique_id){
         this.c_name = c_name;
         this.c_age = c_age;
         this.c_unique_id = c_unique_id;
+    }
+
+    public void setDoses(int doses) {
+        this.doses = doses;
+    }
+
+    public void setDosesPending(int doses) {
+        this.dosesPending = doses;
+    }
+
+    public void DoseDone(){
+        this.dosesPending--;
     }
 }
 class Hospital{
@@ -125,6 +229,18 @@ class Hospital{
         this.h_name = h_name;
         this.h_pincode = h_pincode;
         this.h_unique_id = h_unique_id;
+    }
+
+    public String getH_name() {
+        return h_name;
+    }
+
+    public int getH_pincode() {
+        return h_pincode;
+    }
+
+    public int getH_unique_id() {
+        return h_unique_id;
     }
 }
 class Vaccine{
@@ -159,5 +275,25 @@ class Slot{
         this.s_vaccine = s_vaccine;
         this.s_quantity = s_quantity;
         this.s_hospital_id = s_hospital_id;
+    }
+
+    public int getS_day() {
+        return s_day;
+    }
+
+    public String getS_vaccine() {
+        return s_vaccine;
+    }
+
+    public int getS_quantity() {
+        return s_quantity;
+    }
+
+    public int getS_hospital_id() {
+        return s_hospital_id;
+    }
+
+    public void reduceS_quantity() {
+        this.s_quantity--;
     }
 }
