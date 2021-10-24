e4f37f7904510bc63622e87547cbf57be672a835
Aflah <aflah20082@iiitd.ac.in>
2021-10-24 16:32:58 +0530

    Added Extra Functionality

--- Assignment 3/SnakesandLadders.java
+++ Assignment 3/SnakesandLadders.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public class SnakesandLadders {
+final public class SnakesandLadders {
+    private final String ANSI_RESET;
     final private floor[] floorTracker;
     final private player player;
     final private Dice dice;
@@ -22,8 +23,9 @@ public class SnakesandLadders {
         floorTracker[12] = new normalFloor(12);
         floorTracker[13] = new normalFloor(13);
         player = new player(name);
-        dice = new Dice(2);
+        dice = new Dice();
         points = 0;
+        ANSI_RESET = "\u001B[0m";
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -36,37 +38,110 @@ public class SnakesandLadders {
         }
         System.out.println("Game over");
         System.out.println(snakesandLadders.player.getName() + " accumulated " + snakesandLadders.points + " points");
+        System.out.println();
+        System.out.println("""
+                            Would you like to gamble your current points for a chance at doubling them ? (Y or N)
+                            Rules: 
+                            1) If your points are > 0 then they'll be doubled else you'll get a +1 if you win if they were already less than or equal to 0
+                            2) If your points are > 0 then they'll be turned to 0 if you lose else you'll get a -1 if they were already less than or equal to 0
+                            You can keep playing till you lose""");
+        String choice = sc.nextLine();
+        if (choice.equals("Y")){
+            while (choice.equals("Y")){
+                int prePoints = snakesandLadders.points;
+                snakesandLadders.gamblePoints(sc);
+                System.out.println(snakesandLadders.player.getName() + " accumulated " + snakesandLadders.points + " points");
+                if (snakesandLadders.points > prePoints){
+                    System.out.println("""
+                            Would you like to gamble your current points for a chance at doubling them ? (Y or N)
+                            Rules: 
+                            1) If your points are > 0 then they'll be doubled else you'll get a +1 if you win if they were already less than or equal to 0
+                            2) If your points are > 0 then they'll be turned to 0 if you lose else you'll get a -1 if they were already less than or equal to 0
+                            You can keep playing till you lose""");
+                    choice = sc.nextLine();
+                }
+                else{
+                    System.out.println("Thanks for Playing");
+                    break;
+                }
+            }
+        }
+        else{
+            System.out.println("Thanks for Playing");
+        }
+        System.out.println(snakesandLadders.player.getName() + " accumulated " + snakesandLadders.points + " points");
     }
 
+    private void gamblePoints(Scanner sc){
+        System.out.println("""
+                Choose your bet:\s
+                A for 1
+                B for 2
+                """);
+        String bet = sc.nextLine();
+        dice.roll();
+        String rolled;
+        if (dice.getFaceValue() == 1){
+            rolled = "A";
+        }
+        else {
+            rolled = "B";
+        }
+        if (rolled.equals(bet)){
+            System.out.println("Congrats your points are now doubled");
+            if (points > 0){
+                points = 2*points;
+            }
+            else{
+                points = points++;
+            }
+        }
+        else{
+            System.out.println("Better luck next time");
+            if (points > 0){
+                points = 0;
+            }
+            else{
+                points = points--;
+            }
+        }
+    }
     private void newTurn(Scanner sc, player player, Dice dice) {
+        String ANSI_CYAN = "\u001B[36m";
         if (player.getPosition() == -1){
             while (player.getPosition() == -1){
-                System.out.print("Hit enter to roll the dice");
+                System.out.print(ANSI_CYAN + "Hit enter to roll the dice");
                 String enter = sc.nextLine();
                 dice.roll();
                 int value = dice.getFaceValue();
-                System.out.println("Dice gave " + value);
+                System.out.println("Dice gave " + value + ANSI_RESET);
+                System.out.println();
                 if (value == 1){
                     player.startGame();
                     setPoints(1);
                 }
                 else{
-                    System.out.println("Game cannot start until you get 1");
+                    String ANSI_YELLOW = "\u001B[33m";
+                    System.out.println(ANSI_YELLOW + "Game cannot start until you get 1" + ANSI_RESET);
+                    System.out.println();
                 }
             }
             printInfo();
         }
         else{
-            System.out.print("Hit enter to roll the dice");
+            System.out.print(ANSI_CYAN + "Hit enter to roll the dice" + ANSI_RESET);
             String enter = sc.nextLine();
             dice.roll();
             int value = dice.getFaceValue();
             if (player.getPosition() + value <= 13){
-                System.out.println("Dice gave " + value);
+                System.out.println(ANSI_CYAN + "Dice gave " + value + ANSI_RESET);
+                System.out.println();
             }
+            String ANSI_PURPLE = "\u001B[35m";
             if (player.getPosition() + value > 13){
-                System.out.println("Dice gave 2\n" +
-                        "Player cannot move");
+                System.out.println(ANSI_PURPLE + "Dice gave 2\n" +
+                        "Player cannot move" + ANSI_RESET);
+                System.out.println();
             }
             else{
                 player.move(value);
@@ -74,8 +149,8 @@ public class SnakesandLadders {
                 printInfo();
                 if (!floorTracker[player.getPosition()].floorType().equals("Empty")){
                     if (player.getPosition() + floorTracker[player.getPosition()].jumpBy() > 13){
-                        System.out.println("Dice gave 2\n" +
-                                "Player cannot move");
+                        System.out.println(ANSI_PURPLE + "Dice gave 2\n" +
+                                "Player cannot move" + ANSI_RESET);
                     }
                     else{
                         floorTracker[player.getPosition()].move(player);
@@ -86,21 +161,48 @@ public class SnakesandLadders {
             }
         }
     }
+
     private int getPoints() {
         return points;
     }
+
     private void setPoints(int points){
         this.points = points;
     }
+
     private void gameReady(){
         System.out.println("The game setup is ready");
-    }
-    private void printInfo(){
-        System.out.println("Player position Floor-" + player.getPosition());
-        System.out.println(player.getName() + " has reached an " +
-                floorTracker[player.getPosition()].floorType() + " Floor");
-        System.out.println("Total points " + getPoints());
         System.out.println();
+    }
+
+    private void printInfo(){
+        if (floorTracker[player.getPosition()].floorType().equals("Normal Snake") ||
+                floorTracker[player.getPosition()].floorType().equals("King Cobra")){
+            String ANSI_RED = "\u001B[31m";
+            System.out.println(ANSI_RED + "Player position Floor-" + player.getPosition());
+            System.out.println(player.getName() + " has reached an " +
+                    floorTracker[player.getPosition()].floorType() + " Floor");
+            System.out.println("Total points " + getPoints());
+            System.out.println(ANSI_RESET);
+        }
+        else if (floorTracker[player.getPosition()].floorType().equals("Elevator") ||
+                floorTracker[player.getPosition()].floorType().equals("Ladder")){
+            String ANSI_BLUE = "\u001B[34m";
+            System.out.println(ANSI_BLUE + "Player position Floor-" + player.getPosition());
+            System.out.println(player.getName() + " has reached an " +
+                    floorTracker[player.getPosition()].floorType() + " Floor");
+            System.out.println("Total points " + getPoints());
+            System.out.println(ANSI_RESET);
+        }
+        else{
+            String ANSI_GREEN = "\u001B[32m";
+            System.out.println(ANSI_GREEN + "Player position Floor-" + player.getPosition());
+            System.out.println(player.getName() + " has reached an " +
+                    floorTracker[player.getPosition()].floorType() + " Floor");
+            System.out.println("Total points " + getPoints());
+            System.out.println(ANSI_RESET);
+        }
+
     }
 }
 abstract class floor{
@@ -115,20 +217,38 @@ abstract class floor{
 }
 
 abstract class snakeFloor extends floor{
-    snakeFloor(int location) {
+    private final int decreasePointsBy;
+    private final int decreaseLocationBy;
+    snakeFloor(int location, int decreasePointsBy, int decreaseLocationBy) {
         super(location);
+        this.decreasePointsBy = decreasePointsBy;
+        this.decreaseLocationBy = decreaseLocationBy;
+    }
+    public abstract int reducePoints(int currPoints, int reduceBy);
+
+    public int getDecreasePointsBy() {
+        return decreasePointsBy;
+    }
+
+    public int getDecreaseLocationBy() {
+        return decreaseLocationBy;
     }
 }
 
-class cobraFloor extends snakeFloor{
+final class cobraFloor extends snakeFloor{
 
     cobraFloor(int location) {
-        super(location);
+        super(location, -4, -8);
+    }
+
+    @Override
+    public int reducePoints(int currPoints, int reduceBy) {
+        return currPoints + reduceBy;
     }
 
     @Override
     public void move(player P) {
-        P.setPosition(P.getPosition()-8);
+        P.setPosition(P.getPosition() + this.getDecreaseLocationBy());
     }
 
     @Override
@@ -138,24 +258,29 @@ class cobraFloor extends snakeFloor{
 
     @Override
     public int changePoints(int currPoints) {
-        return currPoints-4;
+        return reducePoints(currPoints, this.getDecreasePointsBy());
     }
 
     @Override
     public int jumpBy() {
-        return -4;
+        return this.getDecreaseLocationBy();
     }
 }
 
-class normalSnakeFloor extends snakeFloor{
+final class normalSnakeFloor extends snakeFloor{
 
     normalSnakeFloor(int location) {
-        super(location);
+        super(location, -2, -4);
+    }
+
+    @Override
+    public int reducePoints(int currPoints, int reduceBy) {
+        return currPoints + reduceBy;
     }
 
     @Override
     public void move(player P) {
-        P.setPosition(P.getPosition()-4);
+        P.setPosition(P.getPosition() + this.getDecreaseLocationBy());
     }
 
     @Override
@@ -165,16 +290,16 @@ class normalSnakeFloor extends snakeFloor{
 
     @Override
     public int changePoints(int currPoints) {
-        return currPoints - 2;
+        return reducePoints(currPoints, this.getDecreasePointsBy());
     }
 
     @Override
     public int jumpBy() {
-        return -2;
+        return this.getDecreaseLocationBy();
     }
 }
 
-class normalFloor extends floor{
+final class normalFloor extends floor{
 
     normalFloor(int location) {
         super(location);
@@ -202,20 +327,38 @@ class normalFloor extends floor{
 }
 
 abstract class ladderFloor extends floor{
-    ladderFloor(int location) {
+    private final int increasePointsBy;
+    private final int increaseLocationBy;
+    ladderFloor(int location, int increasePointsBy, int increaseLocationBy) {
         super(location);
+        this.increasePointsBy = increasePointsBy;
+        this.increaseLocationBy = increaseLocationBy;
+    }
+    public abstract int increaseBy(int currPoints, int reduceBy);
+
+    public int getIncreasePointsBy() {
+        return increasePointsBy;
+    }
+
+    public int getIncreaseLocationBy() {
+        return increaseLocationBy;
     }
 }
 
-class elevatorFloor extends ladderFloor{
+final class elevatorFloor extends ladderFloor{
 
     elevatorFloor(int location) {
-        super(location);
+        super(location, 4, 8);
+    }
+
+    @Override
+    public int increaseBy(int currPoints, int increasePointsBy) {
+        return currPoints + increasePointsBy;
     }
 
     @Override
     public void move(player P) {
-        P.setPosition(P.getPosition()+8);
+        P.setPosition(P.getPosition() + this.getIncreaseLocationBy());
     }
 
     @Override
@@ -225,7 +368,7 @@ class elevatorFloor extends ladderFloor{
 
     @Override
     public int changePoints(int currPoints) {
-        return currPoints + 4;
+        return increaseBy(currPoints, this.getIncreasePointsBy());
     }
 
     @Override
@@ -234,15 +377,15 @@ class elevatorFloor extends ladderFloor{
     }
 }
 
-class normalLadderFloor extends ladderFloor{
+final class normalLadderFloor extends ladderFloor{
 
     normalLadderFloor(int location) {
-        super(location);
+        super(location, 2, 4);
     }
 
     @Override
     public void move(player P) {
-        P.setPosition(P.getPosition()+4);
+        P.setPosition(P.getPosition()+this.getIncreaseLocationBy());
     }
 
     @Override
@@ -252,36 +395,36 @@ class normalLadderFloor extends ladderFloor{
 
     @Override
     public int changePoints(int currPoints) {
-        return currPoints + 2;
+        return increaseBy(currPoints, this.getIncreasePointsBy());
     }
 
     @Override
     public int jumpBy() {
-        return 2;
+        return this.getIncreaseLocationBy();
+    }
+
+    @Override
+    public int increaseBy(int currPoints, int increasePointsBy) {
+        return currPoints + increasePointsBy;
     }
 }
 
-class Dice {
-    private final int numFaces; //maximum face value
-    private int faceValue; //current value showing on the dice
+final class Dice {
+    private final int numFaces;
+    private int faceValue;
 
-    // Constructor: Sets the initial face value.
-    public Dice(int _numFaces) {
-        numFaces = _numFaces;
-        roll();
+    public Dice() {
+        numFaces = 2;
     }
 
-    // Rolls the dice
     public void roll() {
         Random r = new Random();
         int curr_faceValue = 1 + r.nextInt(numFaces);
         setFaceValue(curr_faceValue);
     }
 
-    // Face value setter/mutator.
     private void setFaceValue(int value) {
-        if (value <= numFaces)
-            faceValue = value;
+        faceValue = value;
     }
 
     public int getFaceValue() {
@@ -289,7 +432,7 @@ class Dice {
     }
 }
 
-class player{
+final class player{
     private final String name;
     private int position;
     player(String name) {
